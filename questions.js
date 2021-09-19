633f291fbc29e4850204cd5ea47e89bfe5d90bf7
rashedall <91013231+rashedall@users.noreply.github.com>
2021-09-19 10:11:07 -0700

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,83 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: " يوجد في الأذن مستقبلات كيميائية ",
+    answer: "نعم",
+    options: [
+      "نعم",
+      "لا",
+      "صحيح",
+      "خطا"
+    ]
+  },
+    {
+    numb: 2,
+    question: " يوجد في الأذن مستقبلات كيميائية ",
+    answer: "نعم",
+    options: [
+      "نعم",
+      "لا",
+      "صحيح",
+      "خطا"
+    ]
+  },
+    {
+    numb: 3,
+    question: " يوجد في الأذن مستقبلات كيميائية ",
+    answer: "نعم",
+    options: [
+      "نعم",
+      "لا",
+      "صحيح",
+      "خطا"
+    ]
+  },
+    {
+    numb: 4,
+    question: " يوجد في الأذن مستقبلات كيميائية ",
+    answer: "نعم",
+    options: [
+      "نعم",
+      "لا",
+      "صحيح",
+      "خطا"
+    ]
+  },
+    {
+    numb: 5,
+    question: " يوجد في الأذن مستقبلات كيميائية ",
+    answer: "نعم",
+    options: [
+      "نعم",
+      "لا",
+      "صحيح",
+      "خطا"
+    ]
+  },
+  {
+    numb: 6,
+    question: " يوجد في الأذن مستقبلات كيميائية ",
+    answer: "نعم",
+    options: [
+      "نعم",
+      "لا",
+      "صحيح",
+      "خطا"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
